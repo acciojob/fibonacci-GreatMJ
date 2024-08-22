e0eb9b559eb8ff6adca10a84060670d4a26cd5a9
Mahesh Jaybhaye <97793941+GreatMJ@users.noreply.github.com>
2024-08-22 18:18:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function fibonacci(num) {
-// your code here
-	if(num===0) return 0;
-	if(num<=2) return 1;
+  if (num < 0) {
+        return "Input should be a non-negative integer";
+    }
+    if (num === 0) return 0;
+    if (num === 1 || num === 2) return 1;
 
-	return fibonacci(num-1)+fibonacci(num-2);
+    return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
 module.exports = fibonacci;
